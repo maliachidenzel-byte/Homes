86f51827eabcf10355228ba43008c28c706d6e04
Maliachidenzel-byte <malirashidbusiness@gmail.com>
2026-04-18 23:50:43 +0300

    Added house details page

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-const container = document.getElementById("houses");
+import { db, collection, getDocs } from "./firebase.js";
 
-container.innerHTML = `
-  <div class="house-card">
-    <img src="https://via.placeholder.com/400">
-    <div class="house-info">
-      <h2>Test House</h2>
-      <p>💰 $300</p>
-      <p>📞 123456789</p>
-    </div>
-  </div>
-`;
+let map = L.map('map').setView([-1.286389, 36.817223], 12);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+async function loadHouses() {
+  const querySnapshot = await getDocs(collection(db, "houses"));
+  const container = document.getElementById("houses");
+
+  container.innerHTML = "";
+
+  querySnapshot.forEach((doc) => {
+    const h = doc.data();
+
+    container.innerHTML += `
+      <div class="house-card" onclick='openHouse(${JSON.stringify(h)})'>
+        <img src="${h.image}">
+        <div class="house-info">
+          <h2>${h.title}</h2>
+          <p>💰 ${h.price}</p>
+          <p>📞 ${h.contact}</p>
+        </div>
+      </div>
+    `;
+
+    L.marker([h.lat, h.lng])
+      .addTo(map)
+      .bindPopup(`<b>${h.title}</b><br>${h.price}`);
+  });
+}
+
+window.openHouse = function(house) {
+  localStorage.setItem("house", JSON.stringify(house));
+  window.location.href = "house.html";
+};
+
+loadHouses();
